445c71be06d3f22445ff91ec6080aa1cef667fe7
Sung-Jun Yoon <sjyoon1671@gmail.com>
2018-02-22 17:22:47 +0900

    Update UC_mem_alloc_2_with_size.c

--- UC_mem_alloc_2_with_size.c
+++ UC_mem_alloc_2_with_size.c
@@ -1,6 +1,6 @@
 #include "header.h"
 
-byte_buffer UC_mem_alloc_2_with_size(size_t row, size_t col, BYTE Initail_value)
+byte_buffer UC_mem_alloc_2_with_size(size_t row, size_t col, BYTE Initial_value)
 {
 	size_t i = 0;
 	byte_buffer res;
@@ -10,7 +10,7 @@ byte_buffer UC_mem_alloc_2_with_size(size_t row, size_t col, BYTE Initail_value)
 	res.buf = (BYTE **)malloc(res.row * sizeof(BYTE*));
 	for (i = 0; i<res.row; i++) {
 		res.buf[i] = (BYTE *)malloc(res.col * sizeof(BYTE));
-		memset(res.buf[i], Initail_value, res.col * sizeof(BYTE));
+		memset(res.buf[i], Initial_value, res.col * sizeof(BYTE));
 	}
 
 	return res;
